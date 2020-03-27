bd1a6c7aa71c1bbaf0d103a20f93c211136ac7e9
roshni-rawat-cs18 <59726095+roshni-rawat-cs18@users.noreply.github.com>
2020-03-27 23:39:13 +0530

    Update MyBinarySearchTree.java
    Create a class of MyBinarSearchTree to insert data

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,42 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+  TreeNode root;
+  int count= 0;
+  public MyBinarySearchTree(){
+    root =null;
+    count=0;
+    }
+  public void insert(int data){
+    TreeNode tn= new TreeNode();
+    tn.setdata(data);
+    if(root==null){
+      root=tn;
+      }
+    else{
+      TreeNode current=root;
+      while(true){
+        if(data>current.getdata()){
+          current.setRight(tn);
+          break;
+          }
+        else{
+          current=current.getRight();
+          }
+        }
+      else{
+        if(current.getLeft()==null){
+          current.setLeft(tn);
+          break;
+          }
+        else{
+          current=current.getleft();
+          }
+        }
+      }
+    }
+
 }

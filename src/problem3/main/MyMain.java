e61b7d7173e851fcc746ae70d899a913e22af7ea
roshni-rawat-cs18 <59726095+roshni-rawat-cs18@users.noreply.github.com>
2020-03-28 07:42:10 +0530

    Update MyMain.java
    Create a main class of queue

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.muqueue.MyPriorityQueue;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+  public static void main (String [] args){
+    MyPriorityQueue obj=new MyPriorityQueue();
+    obj.insert("roshni",1);
+    obj.insert("riya",4);
+    obj.insert("rahul",3);
+    obj.insert("unnati",2);
+    obj.insert("saumya",5);
+    obj.display();
+    }
+  
 }

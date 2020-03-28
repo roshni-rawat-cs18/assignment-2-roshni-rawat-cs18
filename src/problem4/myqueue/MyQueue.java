4a89d1bcab83342dfaa355a8a4d56a7bac3866d3
roshni-rawat-cs18 <59726095+roshni-rawat-cs18@users.noreply.github.com>
2020-03-28 12:59:58 +0530

    Update MyQueue.java
    Complete a myqueue class

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -5,7 +5,63 @@
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
+import org.w3c.dom.Node;
+import problem1.node.TreeNode;
+
 // to create queue to store pre - order successor
 
 public class MyQueue {
-}
+  class Node{
+    int data;
+    Node next;
+    public void setData(int data){
+      this.data=data;
+      }
+    public void setNext(Node next){
+      this.next=next;
+      }
+    public int getData(){
+      return data;
+      }
+    
+    public Node getNext(){
+      return next;
+      
+      }
+    }
+  Node front;
+  Node rear;
+  public MyQueue(){
+    front = null;
+    rear = null;
+    }
+  public void enqueue(int data){
+    Node tn=new Node();
+    tn.setData(data);
+    if(front==null){
+      front=tn;
+      rear=tn;
+      }
+    }
+  public void preSuccessor(TreeNode root){
+    if(root==null)
+      return;
+    else{
+      enqueue(root.getdata());
+      preSuccessor(root.getLeft());
+      preSuccessor(root.getRight());
+      }
+    }
+  public void getpreSucc(){
+    Node temp=front.getNext();
+    whil(temp!=null){
+      System.out.println(temp.getData+"\t");
+      temp=temp.getNext();
+      }
+    }
+  }
+        
+      
+  
+                   
+

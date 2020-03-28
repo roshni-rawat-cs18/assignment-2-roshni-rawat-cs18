cd268841ff68391bbe6e3337f107aaaad0651df6
roshni-rawat-cs18 <59726095+roshni-rawat-cs18@users.noreply.github.com>
2020-03-28 07:57:09 +0530

    Update MyPriorityQueue.java
    Create a insert function in MyPriorityQueue class

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -5,6 +5,34 @@
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
-
+import problem3.node.Node;
 public class MyPriorityQueue {
+  Node head;
+  public MyPriorityQueue(){
+    head=null;
+    }
+  public void insert(String name,int roll){
+    Node tn= new Node();
+    tn.setName(name);
+    Node temp=head;
+    if(head==null){
+      head=tn;
+      }
+    else{
+      if(head.getRight()>roll){
+        tn.setNext(head);
+        head=temp;
+        }
+      else{
+        while(temp.getNext()!=null&&temp.getNext().getRoll()<roll){
+          temp=temp.getNext();
+          }
+        tn.setNext(temp.getNext());
+        temp.setNext(tn);
+        }
+      }
+    
+  
+    
+      
 }
